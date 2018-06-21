5ae972ce1a15d05d27b721512082255374f2555a
kamalay kishore <kamalay.kishore@gmail.com>
2018-06-20 20:35:53 -0500

    firstchange

--- NewSpring/src/main/java/io/springboot/Api/NewSpring/App.java
+++ NewSpring/src/main/java/io/springboot/Api/NewSpring/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Hello world!
  *
  */
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScanfrf5f5
 public class App 
 {
     public static void main( String[] args )
